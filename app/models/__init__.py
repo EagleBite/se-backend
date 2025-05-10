9964bbd7c9c6e157e571ece35203a8078b5b3c3f
jyd777 <jiangyidong2004715@gmail.com>
2025-05-10 13:49:01 +0800

    update init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,6 @@ from .car import Car
 from .manager import Manager
 from .order import Order
 from .order_participant import OrderParticipant
+from .conversation import Conversation
 
-__all__ = ['User', 'Car', 'Manager', 'Order', 'OrderParticipant']
+__all__ = ['User', 'Car', 'Manager', 'Order', 'OrderParticipant','Conversation']
